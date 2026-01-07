4b653a82fd5d5c38b9706a13587bf475caef53e9
Fathum Mubin <fathummubin26@gmail.com>
2026-01-08 02:42:35 +0600

    Create simple_calculator.php

---
+++ simple_calculator.php
@@ -0,0 +1,12 @@
+<?php
+$a = 20;
+$b = 5;
+$op = "*";
+
+switch ($op) {
+    case "+": echo $a + $b; break;
+    case "-": echo $a - $b; break;
+    case "*": echo $a * $b; break;
+    case "/": echo $a / $b; break;
+}
+?>
